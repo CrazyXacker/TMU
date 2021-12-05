3e80103d9fbb527b533a488e8ac15b7c0adba2ea
CrazyXacker <crazyxacker@gmail.com>
2021-12-05 13:28:51 +0200

    Add StringListAdapter for genres field in BookInfo. Description field in BookInfo

---
+++ src/main/java/com/crazyxacker/apps/tmu/adapters/StringListAdapter.java
@@ -0,0 +1,21 @@
+package com.crazyxacker.apps.tmu.adapters;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class StringListAdapter implements JsonDeserializer<String> {
+
+    @Override
+    public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        try {
+            return context.deserialize(json, typeOfT);
+        } catch (Exception ignored) {
+            // Maybe incorrect list
+            return null;
+        }
+    }
+}
