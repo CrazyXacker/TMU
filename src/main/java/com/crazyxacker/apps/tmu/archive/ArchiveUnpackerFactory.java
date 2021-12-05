c1ca0a4d03880147881de9af941874229b0b12a6
CrazyXacker <1788475+CrazyXacker@users.noreply.github.com>
2021-12-05 13:35:59 +0200

    Always create Zip unpacker for zip archives

--- src/main/java/com/crazyxacker/apps/tmu/archive/ArchiveUnpackerFactory.java
+++ src/main/java/com/crazyxacker/apps/tmu/archive/ArchiveUnpackerFactory.java
@@ -1,16 +1,15 @@
 package com.crazyxacker.apps.tmu.archive;
 
 import com.crazyxacker.apps.tmu.helpers.ContentDetector;
-import com.crazyxacker.apps.tmu.utils.NativeUtils;
 
 import java.io.File;
 
 public class ArchiveUnpackerFactory {
 
     public static IArchiveUnpacker create(File file) {
-        if (NativeUtils.isNativeImage() && ContentDetector.isZipFile(file)) {
+        if (ContentDetector.isZipFile(file)) {
             return Zip.create();
-        } else if (ContentDetector.isArchiveFile(file)) {
+        } else if (ContentDetector.isSevenZipOrRarFile(file)) {
             return SevenZip.create();
         }
         return null;
