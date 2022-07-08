79e5ad2bad97245d08a26f34a65cbef4ebdf4662
CrazyXacker <1788475+CrazyXacker@users.noreply.github.com>
2022-07-08 21:22:52 +0300

    Saving chapter titles into properties. Using this titles in Telegram post generation

--- src/main/java/com/crazyxacker/apps/tmu/utils/TelegramUtils.java
+++ src/main/java/com/crazyxacker/apps/tmu/utils/TelegramUtils.java
@@ -1,24 +1,28 @@
 package com.crazyxacker.apps.tmu.utils;
 
 import com.crazyxacker.apps.tmu.controller.MainController;
+import com.crazyxacker.apps.tmu.models.UploadInfo;
 
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelegramUtils {
 
-    public static String createTelegramPostText(String title, String tags, String description, String url) {
-        boolean addEllipsize = description.length() > 700;
-        String newDescription = StringUtils.isNotEmpty(description)
-                ? String.format("\n\n__%s%s__", description.substring(0, 700), (addEllipsize ? "..." : ""))
+    public static String createTelegramPostText(UploadInfo info) {
+        boolean addEllipsize = info.getDescription().length() > 700;
+        String newDescription = StringUtils.isNotEmpty(info.getDescription())
+                ? String.format("\n\n__%s%s__", info.getDescription().substring(0, 700), (addEllipsize ? "..." : ""))
                 : "";
 
-        String linksNewLineSeparated = ArrayUtils.splitString(url, ",")
-                .stream()
-                .map(String::trim)
-                .collect(Collectors.joining("\n"));
+        String tagsLine = StringUtils.join(",", info.getTags());
 
-        return StringUtils.isNotEmpty(tags)
-                ? String.format("**%s**\n\n%s%s\n\n%s", title, MainController.convertsTagsIntoHashtags(tags), newDescription, linksNewLineSeparated)
-                : String.format("**%s**\n\n%s%s", title, newDescription, url);
+        List<String> links = new ArrayList<>();
+        for (int i = 0; i < info.getLinks().size(); i++) {
+            links.add(String.format("%s %s", info.getTitles().get(i), info.getLinks().get(i)));
+        }
+
+        return StringUtils.isNotEmpty(tagsLine)
+                ? String.format("**%s**\n\n%s%s\n\n%s", info.getTitle(), MainController.convertsTagsIntoHashtags(tagsLine), newDescription, StringUtils.join("\n", links))
+                : String.format("**%s**\n\n%s%s", info.getTitle(), newDescription, StringUtils.join("\n", links));
     }
 }
